f13f3919c7ae720889796112adf36f91697c18b6
Thx_Lia <jonathan.lia@outlook.com>
2021-04-13 15:49:58 +0800

    第一个测试发行版本

---
+++ Rhinoceros.MVVM/BaseModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Rhinoceros.MVVM
+{
+    public class BaseModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+        protected void OnPropertyChanged(PropertyChangedEventArgs e) => PropertyChanged(this, e);
+
+        public void RaisePropertyChanged([CallerMemberName] string propertyName = null) => OnPropertyChanged(new PropertyChangedEventArgs(propertyName));
+
+        public bool SetProperty<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (Equals(field, value)) return false;
+
+            field = value;
+            RaisePropertyChanged(propertyName);
+            return true;
+        }
+    }
+}
